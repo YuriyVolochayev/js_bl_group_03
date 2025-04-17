b88b101735f02db6a4a4e87996fb3ccf888fdc9c
volochayev <stingray8888831@gmail.com>
2025-04-17 21:49:24 +0300

    test

---
+++ js/lesson-1.js
@@ -0,0 +1,91 @@
+// Якщо змінна a дорівнює 10, виведіть 'Вірно', інакше виведіть 'Невірно' через тернарник
+
+// const a === 10;
+// const b = a === 10 ? "Вірно" : "Невірно";
+
+// console.log(b);
+
+
+// У змінній min лежить число від 0 до 59. Визначте у котру чверть години
+// Попадає це число (у першу, другу, третю або четверту).
+
+// let min;
+//  if (min<=15) {
+//    alert "1"
+//  } else if (15 <= min <= 30) {
+//      alert "2"
+    
+//  } else if (30 <= min <= 45) {
+//      alert "3"
+// }
+ 
+// else if (min > 15 && min <= 30)
+
+// Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
+// значення '1', то у змінну result запишемо 'зима', якщо має значення
+// '2' - 'весна' і так далі. Розв'яжіть завдання через switch-case.
+
+// let result;
+// switch (ssn) {
+//     case "1":
+//         result = "зима";
+//         console.log("зима")
+//         break;
+//     case "2":
+//         result = "весна";
+//         console.log("весна")
+//         break;
+//     case "3":
+//         result = "літо";
+//         console.log("літо")
+//         break;
+//     case "4":
+//         result = "осінь";
+//         console.log("осінь")
+//         break;
+//     default:
+//         console.log("пупупу");
+
+//         const num = 2;
+// let result = null;
+// switch (num) {
+//     case 1:
+//         result = "winter";
+//         break;
+//     case 2:
+//         result = "spring";
+//         break;
+//     case 3:
+//         result = "summer";
+//         break;
+//     case 4:
+//         result = "fall";
+//         break;
+//     default:
+//         result = "not a season";
+// }
+
+
+
+
+// скористаємося циклом while та виведіть у консоль числа від 0 до 50
+// let i = 0;
+// while (i <= 50) {
+//   console.log(i);
+//   i+=1;
+// }
+
+
+//За допомогою циклу for додайте всі парні числа від min до max
+
+let min = 0;
+let max = 50;
+let sum = 0;
+
+for (let i = min; i <= max; i++) {
+  if (i % 2 === 0) {
+    sum += i;
+  }
+}
+
+console.log(sum);
---
+++ js/test-1.js
@@ -0,0 +1,62 @@
+// TASK1
+
+// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, створений із цього рядка.
+
+// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
+// Усі символи slug повинні бути в нижньому регістрі.
+// Усі слова slug повинні бути розділені тире.
+    
+    
+    
+    
+    
+// console.log(slugify("Arrays for beginners"));
+// console.log(slugify("English for developer"));
+// console.log(slugify("Ten secrets of JavaScript"));
+// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+
+
+// TASK2
+
+
+// Напиши функцію під назвою makeArray, яка приймає три параметри: firstArray (масив), secondArray (масив) і maxLength (число). Функція повинна створювати новий масив, який містить усі елементи з firstArray, а потім усі елементи з secondArray.
+
+// Якщо кількість елементів у новому масиві перевищує maxLength, функція повинна повернути копію масиву з довжиною maxLength елементів.
+// В іншому випадку функція повинна повернути весь новий масив.
+    
+    
+    
+    
+    
+    
+    
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+
+
+// TASK3
+
+
+// Напиши функцію filterArray(numbers, value), яка приймає масив чисел (numbers) та значення (value) як параметри. Функція повинна повертати новий масив лише тих чисел із масиву numbers, які більші за значення value.
+
+
+
+// Усередині функції:
+
+// Створи порожній масив, у який будеш додавати підходящі числа.
+// Використай цикл для ітерації кожного елемента масиву numbers.
+// Використовуй умовний оператор if усередині циклу для перевірки кожного елемента и додавання до свого масиву.
+// Поверни свій новий масив з підходящими числами як результат.
+
+
+
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
