72c6b3a044f4d034136a2fdb2282df01657adc87
volochayev <stingray8888831@gmail.com>
2025-04-30 08:29:02 +0300

    task 1-7

---
+++ js/lection.js
@@ -0,0 +1,32 @@
+
+// function greet(name) {
+//   return `Welcome ${name}!`;
+// }
+
+// console.log(greet("Mango"));
+
+// console.log(greet);
+
+function greet(name) {
+  console.log(`Welcome ${name}!`);
+}
+
+// function notify(name) {
+//   console.log(`Dear ${name}, your room will be ready in 30 minutes`);
+// }
+// function registerGuest(name, callback) {
+//   console.log(`Registering ${name}!`);
+//   callback(name);
+// }
+// function greet(name) {
+//   console.log(`Welcome ${name}!`);
+// }
+
+// function notify(name) {
+//   console.log(`Dear ${name}, your room will be ready in 30 minutes`);
+// }
+
+// function registerGuest(name, callback) {
+//   console.log(`Registering ${name}!`);
+//   callback(name);
+// }
--- js/test-1.js
+++ js/test-1.js
@@ -241,3 +241,24 @@
 // const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
 // const bestScore = Math.max (...allScores);
 // const worstScore = Math.min(...allScores);
+
+
+// Функція filterArray(numbers, value) приймає першим параметром масив чисел numbers і повертає новий масив,
+// в якому будуть тільки ті елементи оригінального масиву, які більші за значення другого параметра числа value.
+// Якщо таких значень не буде знайдено, функція повертає порожній масив.
+
+// Виконай рефакторинг функції таким чином, щоб замість циклу for, вона використовувала метод forEach.
+
+// function filterArray(numbers, value) {
+//   const newArray = [];
+
+//   numbers.forEach(function (number) {
+//     if (number > value) {
+//       newArray.push(number);
+//     }
+//   });
+
+//   return newArray;
+// }
+
+
