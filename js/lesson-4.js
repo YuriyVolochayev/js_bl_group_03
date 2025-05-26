dd8a93a0bee4c16fb9d62d1752b3b79f28f98b7a
volochayev <stingray8888831@gmail.com>
2025-05-26 16:36:37 +0300

    comleate some task

---
+++ js/lesson-4.js
@@ -0,0 +1,33 @@
+// //TODO: № 1 ============================================== // Натискання на
+// кнопку "SHOW ME" має виводити значення з поля введення (дивіться на елементи
+//     в html-розмітці)
+
+// const btnClick = document.querySelector("#alertButton");
+// const input = document.querySelector("#alertInput");
+
+// btnClick.addEventListener("click", showValue);
+
+// function showValue() {
+//     return alert(input.value);
+// }
+
+
+//TODO: № 2 ==============================================
+// Після натискання кнопки "SWAP ME" здійснюється обмін вмістом між двома інпутами.
+// Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
+
+// const btnClick = document.querySelector("#swapButton");
+// const leftSwapInput = document.querySelector("#leftSwapInput");
+// const rightSwapInput = document.querySelector("#rightSwapInput");
+
+// btnClick.addEventListener("click", clickToSwap);
+
+// function clickToSwap() {
+//     const swap = leftSwapInput.value;
+//     leftSwapInput.value = rightSwapInput.value;
+//     rightSwapInput.value = swap;
+//     };
+
+
+//TODO: № 3 ==============================================
+// При кліку на кнопку текст параграфів змінюється на їх порядковий номер
